9eb4355470ee40b8dbb91c3fa3b88947fc3f17cd
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-12 16:28:11 +0530

    handling error state

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -6,7 +6,7 @@ import Button from "./Button";
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
-      <div className={classes.backdrop} />
+      <div className={classes.backdrop} onClick={props.onOkay} />
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -15,7 +15,7 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button>Okay</Button>
+          <Button onClick={props.onOkay}>Okay</Button>
         </footer>
       </Card>
     </React.Fragment>
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,6 +7,7 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredAge, setEnteredAge] = useState(0);
+  const [error, setError] = useState({title: '', message: ''});
 
   const usernameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
@@ -18,8 +19,20 @@ const AddUser = (props) => {
 
   const AddUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUserName.trim().length === 0 || +enteredAge.trim().length < 1)
+    if (enteredUserName.trim().length === 0 || +enteredAge.trim() === 0) {
+      setError({
+        title: 'Invalid Input',
+        message: 'Please enter a valid name and age (non-empty)'
+      });
       return;
+    }
+    if (+enteredAge.trim() < 1) {
+      setError({
+        title: 'Invalid Age',
+        message: 'Please enter a valid age (age > 1)'
+      });
+      return;
+    }
     console.log(enteredUserName, enteredAge);
     setEnteredAge(0);
     setEnteredUserName("");
@@ -31,9 +44,13 @@ const AddUser = (props) => {
     props.onAddUser(usr);
   };
 
+  const handleModalClose = event => {
+    setError({title: '', message: ''});
+  }
+
   return (
     <React.Fragment>
-      <ErrorModal title="An Error occured" message="Something went wrong" />
+      {error && error.title.length > 0 && <ErrorModal title={error.title} message={error.message} onOkay={handleModalClose} />}
       <Card className={classes.input}>
         <form onSubmit={AddUserHandler}>
           <label htmlFor="username">User name</label>
