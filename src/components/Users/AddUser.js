4cfa7a2d53b77ce7e800efdce0151fb51a8bf766
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-12 16:14:11 +0530

    creating error modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,45 +1,60 @@
-import React, { useState } from 'react';
-import Card from '../UI/Card';
-import classes from './AddUser.module.css';
-import Button from '../UI/Button';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = props => {
-    const [enteredUserName, setEnteredUserName] = useState('');
-    const [enteredAge, setEnteredAge] = useState(0);
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState(0);
 
-    const usernameChangeHandler = event => {
-        setEnteredUserName(event.target.value);
-    }
+  const usernameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
 
-    const ageChangeHandler = event => {
-        setEnteredAge(event.target.value);
-    }
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
 
-    const AddUserHandler = event => {
-        event.preventDefault();
-        if (enteredUserName.trim().length === 0 || +enteredAge.trim().length < 1) return;
-        console.log(enteredUserName, enteredAge);
-        setEnteredAge(0);
-        setEnteredUserName('');
-        const usr = {
-            name: enteredUserName,
-            age: enteredAge,
-            id: Math.random().toString(),
-        };
-        props.onAddUser(usr);
-    }
+  const AddUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUserName.trim().length === 0 || +enteredAge.trim().length < 1)
+      return;
+    console.log(enteredUserName, enteredAge);
+    setEnteredAge(0);
+    setEnteredUserName("");
+    const usr = {
+      name: enteredUserName,
+      age: enteredAge,
+      id: Math.random().toString(),
+    };
+    props.onAddUser(usr);
+  };
 
-    return (
-        <Card className={classes.input}>
+  return (
+    <React.Fragment>
+      <ErrorModal title="An Error occured" message="Something went wrong" />
+      <Card className={classes.input}>
         <form onSubmit={AddUserHandler}>
-            <label htmlFor='username'>User name</label>
-            <input id='username' type="text" value={enteredUserName} onChange={usernameChangeHandler} />
-            <label htmlFor='age'>Age (in years)</label>
-            <input id='age' type="number" value={enteredAge} onChange={ageChangeHandler} />
-            <Button type='submit'>Add User</Button>
+          <label htmlFor="username">User name</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUserName}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (in years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
         </form>
-        </Card>
-    )
+      </Card>
+    </React.Fragment>
+  );
 };
 
 export default AddUser;
